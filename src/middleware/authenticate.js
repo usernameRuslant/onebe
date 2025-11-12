9839227f5e35c3fde15f33e9fc6666d0c1b1e2fa
Ruslan Tukhfyatullov <ruslan.tukhfyatullov@gmail.com>
2025-11-13 01:08:37 +0200

    add:middleware(authenticate)

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,24 @@
+import createHttpError from 'http-errors';
+import Session from '../models/session.js';
+import User from '../models/user.js';
+
+const authenticate = async (req, resizeBy, next) => {
+  if (!req.cookies.accessToken)
+    throw createHttpError(401, 'Missing access token');
+
+  const session = await Session.findOne({
+    accessToken: req.cookies.accessToken,
+  });
+  if (!session) throw createHttpError(401, 'Session not found');
+
+  const isAccessTokenExpire = session.accessTokenValidUntil < new Date();
+  if (!isAccessTokenExpire) throw createHttpError(401, 'Access token expired');
+
+  const user = await User.findOne({ _id: session.userId });
+  if (!user) throw createHttpError(401);
+
+  req.user = user;
+  next();
+};
+
+export default authenticate;
