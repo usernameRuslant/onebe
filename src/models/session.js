b9546470a660681f7db5df866c2ca704ba5a1235
Ruslan Tukhfyatullov <ruslan.tukhfyatullov@gmail.com>
2025-11-13 00:30:13 +0200

    add Session model

---
+++ src/models/session.js
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose';
+
+const sessionSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    accessTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+const Session = model('session', sessionSchema);
+export default Session;
