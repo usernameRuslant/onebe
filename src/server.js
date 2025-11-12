ed6d110919cf3399574cf3250728c356ff442e01
Ruslan Tukhfyatullov <ruslan.tukhfyatullov@gmail.com>
2025-11-13 00:17:14 +0200

    add User model

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
 
+import { connectMongoDB } from './db/connectMongoDB.js';
+
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
+await connectMongoDB();
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
